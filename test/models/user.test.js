d37b4df25507b0899acc15624e8458340fdce64d
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 16:50:20 -0500

    agregando mis datos

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,19 +3,19 @@ describe ("Unit Test for User class", () =>{
 
     test('Create an User object', () => {
         //Aqui invocas el codigo que vas a usar en tu app.
-        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated");
+        const user = new User(1, "sergioaltuzar", "Sergio", "Bio", "dateCreated", "lastUpdated");
         //aqui validas los resultados de ese codigo para
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
         expect(user.id).toBe(1);
-        expect(user.username).toBe("carlogilmar");
-        expect(user.name).toBe("Carlo");
+        expect(user.username).toBe("sergioaltuzar");
+        expect(user.name).toBe("Sergio");
         expect(user.bio).toBe("Bio");
         expect(user.dateCreated).not.toBeUndefined(); //verifica que el valor no sea undefined
         expect(user.lastUpdated).not.toBeUndefined(); //verifica que el valor no sea undefined
     });
     test('Add getters', () => {
         //Aqui invocas el codigo que vas a usar en tu app.
-        const user = new User(1, "carlogilmar", "Carlo", "Bio");
+        const user = new User(1, "sergioaltuzar", "Sergio", "Bio");
         //aqui validas los resultados de ese codigo para
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
         expect(user.getUsername).toBe("carlogilmar");
