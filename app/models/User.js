78adf7785fbf4a5e97dffb3bd4fee6ee43fb5c15
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 16:58:08 -0500

    agregando codigo para prueba
    agregando codigo  y corriendo corriendo prueba passed

--- app/models/User.js
+++ app/models/User.js
@@ -19,6 +19,13 @@ class User {
     get getLastUpdated() {     
         return this.lastUpdated;
     }
+
+    set setUsername(newUsername) {
+        this.username = newUsername;
+    }
+    set setBio(newBio) {
+        this.bio = newBio;
+    }
 }
 
 module.exports = User;
