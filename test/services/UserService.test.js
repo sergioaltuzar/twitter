df6c6dcec17ba34a90f511546889520f972444ae
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 17:54:11 -0500

    correccion test passed

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -2,7 +2,7 @@ const UserService = require('./../../app/services/UserService');
 
 describe("Test for UserService", () => {
     test("1. Create a new user using the UserService", () => {
-        const user = UserService.create(1, "sergioaltuz", "SergIO");
+        const user = UserService.create(1, "sergioaltuzar", "Sergio");
         expect(user.username).toBe("sergioaltuzar");
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
