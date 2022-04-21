5fc864e5f6127d50c71485c2bdf8f9e37aa2851f
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 18:33:44 -0500

    userservice passed

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -3,6 +3,10 @@ class UserService {
     static create (id, username, name){
         return new User(id, username, name, "Sin Bio");
     };
-};
+
+    static getInfo(user){
+      return [user.id, user.username, user.name, user="Sin bio"]
+    }
+} 
 
 module.exports = UserService;
