2374d5e4ecc52200bcd0d83046dabe22e466d7b6
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 18:09:15 -0500

    requrimientos prueba failed

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -7,4 +7,12 @@ describe("Test for UserService", () => {
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
     })
+    test("2. Get all user data in a list", () => {
+        const user = UserService. create(1, "sergioaltuzar", "Sergio");
+        const userInfoInList = UserService.getInfo(user);
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("sergioaltuzar");
+        expect(userInfoInList[2]).toBe("Sergio");
+        expect(userInfoInList[3]).toBe("Sin bio");
+    })
 })
