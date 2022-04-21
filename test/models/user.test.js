6761bc07a97ae0347270453f88f6bbb2030ef00e
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 16:40:44 -0500

    agregando codigo, prueba passed

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -13,4 +13,14 @@ describe ("Unit Test for User class", () =>{
         expect(user.dateCreated).not.toBeUndefined(); //verifica que el valor no sea undefined
         expect(user.lastUpdated).not.toBeUndefined(); //verifica que el valor no sea undefined
     });
+    test('Add getters', () => {
+        //Aqui invocas el codigo que vas a usar en tu app.
+        const user = new User(1, "carlogilmar", "Carlo", "Bio");
+        //aqui validas los resultados de ese codigo para
+        //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
+        expect(user.getUsername).toBe("carlogilmar");
+        expect(user.getBio).toBe("Bio");
+        expect(user.getDateCreated).not.toBeUndefined(); //verifica que el valor no sea undefined
+        expect(user.getLastUpdated).not.toBeUndefined();
+    });
 });
