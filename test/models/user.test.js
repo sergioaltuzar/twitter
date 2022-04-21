a57ebd7ad0af26c06760ab2593341b5d5973550a
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 16:12:17 -0500

    segunda prueba passed

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,16 +1,16 @@
-const User = require('./../../app/models/user');
+const User = require('../../app/models/user');
 describe ("Unit Test for User class", () =>{
 
     test('Create an User object', () => {
         //Aqui invocas el codigo que vas a usar en tu app.
-        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreaded", "lastUpdated");
+        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated");
         //aqui validas los resultados de ese codigo para
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
         expect(user.id).toBe(1);
         expect(user.username).toBe("carlogilmar");
         expect(user.name).toBe("Carlo");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBer("dateCreated");
-        expect(user.lastUpdated).toBer("lastUpdated");
+        expect(user.dateCreated).toBe("dateCreated");
+        expect(user.lastUpdated).toBe("lastUpdated");
     });
 });
