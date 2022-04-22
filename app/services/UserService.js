430467458545a639217885c7f5635a72d6b77f3e
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 19:41:46 -0500

    update userservice passed

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -10,6 +10,9 @@ class UserService {
     static updateUserUsername(user, newUsername) {
         return user.username= newUsername;
     }
+    static getAllUsernames([user1, user2, user3]){
+        return [user1.username, user2.username, user3.username];
+    }
 } 
 
 module.exports = UserService;
