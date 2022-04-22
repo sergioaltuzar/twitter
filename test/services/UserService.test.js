35846220ab89e84beddf88264cd750a83ec4e562
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 19:40:28 -0500

    update test4 failed

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -6,7 +6,7 @@ describe("Test for UserService", () => {
         expect(user.username).toBe("sergioaltuzar");
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
-    })
+    });
     test("2. Get all user data in a list", () => {
         const user = UserService. create(1, "sergioaltuzar", "Sergio");
         const userInfoInList = UserService.getInfo(user);
@@ -14,11 +14,19 @@ describe("Test for UserService", () => {
         expect(userInfoInList[1]).toBe("sergioaltuzar");
         expect(userInfoInList[2]).toBe("Sergio");
         expect(userInfoInList[3]).toBe("Sin bio");
-    })
+    });
     test("3. Update username", () => {
         const user = UserService. create(1, "sergioaltuzar", "Sergio");
         UserService.updateUserUsername(user, "Serch")
         expect(user.username).toBe("Serch");
-    })
-    
-})
+    });
+    test("4. Given a list of users give me the list of usernames", () => {
+        const user1 = UserService. create(1, "sergioaltuzar1", "Sergio");
+        const user2 = UserService. create(1, "sergioaltuzar2", "Sergio");
+        const user3 = UserService. create(1, "sergioaltuzar3", "Sergio");
+        const usernames = UserService.getAllUsernames ([user1, user2, user3]);
+        expect(usernames).toContain("sergioaltuzar1");
+        expect(usernames).toContain("sergioaltuzar2");
+        expect(usernames).toContain("sergioaltuzar3");
+    });
+});
