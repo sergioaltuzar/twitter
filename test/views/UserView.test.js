826c0fdfd7e1d0ae3df09a46fc07f72e641dd929
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 10:57:30 -0500

    agregando 2,3,4

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,12 +1,29 @@
-const UserView = require('./../../app/views/UserView');
+const UserView = require('./../../app/views/userView');
 
-describe('Test for UserView', () => { 
+describe('Test for UserView', () => { {
 
-    // Valida que al enviar en el `payload` un valor `null`, obtenga un objeto con la llave `error` y el valor indique `payload no existe`.
-    test('Return an error object when try to creat a new user whit an null playload', () => { 
-        const playload = null;
-        const result = UserView.createUser(playload);
+}
+    test('1.) Return an error object when try to create a new user with an null payload', () => {
+        const payload = null
+        const result = UserView.createUser(payload);
         //https://jestjs.io/docs/using-matchers#strings
-        expect(result.error).toMatch(/playload no existe/);
+        expect(result.error).toMatch(/payload no existe/);
+    })
+    test('2.) Return an error object when try to create a new user with a payload with invalid propierties', () => {
+        const payload = {username: null, name: '12', id: 'id'}
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    })
+    test('3.) Return an error object when try to create a new user with missing propierties', () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    });
+    test('4.) Create a user by a given valid payload', () => {
+        const payload = {name: "name", username: "username", id: 1};
+        const result = UserView.createUser(payload);
+        expect(result.name).toBe("name");
+        expect(result.username).toBe("username");
+        expect(result.id).toBe(1);
     });
 });
