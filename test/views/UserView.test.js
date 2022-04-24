b0ba1f5e747b9f82b9cfb8390ee0a3df6d83b252
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 11:01:10 -0500

    test1 payload no existe

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -10,17 +10,17 @@ describe('Test for UserView', () => { {
         expect(result.error).toMatch(/payload no existe/);
     })
     test('2.) Return an error object when try to create a new user with a payload with invalid propierties', () => {
-        const payload = {username: null, name: '12', id: 'id'}
+        const payload = {username: null, name: '12', id: 'id'};
         const result = UserView.createUser(payload);
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     })
     test('3.) Return an error object when try to create a new user with missing propierties', () => {
-        const payload = {username: "Username"}
-        const result = UserView.createUser(payload)
+        const payload = {username: "Username"};
+        const result = UserView.createUser(payload);
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     });
     test('4.) Create a user by a given valid payload', () => {
-        const payload = {name: "name", username: "username", id: 1};
+        const payload = {username: "username", id: 1, name: "name"};
         const result = UserView.createUser(payload);
         expect(result.name).toBe("name");
         expect(result.username).toBe("username");
