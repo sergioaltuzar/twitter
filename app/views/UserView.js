33bb69bf8f558440c8dc01426d8afd46eb801592
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-24 11:03:49 -0500

    todas pruebas passed

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,12 +1,19 @@
-const UserService = require('./../services/UserService')
+const UserService = require('./../services/userService');
 
-class UserView{
-
-    static createUser(playload){
-        if(playload == null){
-            return {error: "playload no existe"};
+class UserView {
+    static createUser(payload) {
+        const keys =['username','id','name']
+        if (payload === null){
+            return {error:"payload no existe"}
+         }
+        else if(Object.keys(payload).some(key => payload[key] === null)){
+            return {error:"necesitan tener un valor válido"}
         }
+        if(!keys.every(key=>Object.keys(payload).includes(key))){
+            return {error:"necesitan tener un valor válido"}
+        }
+        const newUser = UserService.create(payload.id,payload.username,payload.name)
+        return {name:newUser.name , username:newUser.username , id:newUser.id}
     }
 }
-
 module.exports = UserView;
